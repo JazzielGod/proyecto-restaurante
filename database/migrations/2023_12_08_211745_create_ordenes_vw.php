75f276613bca5443fe22196bace35216c4a3dba4
JazzielGod <jazzielbh@gmail.com>
2025-04-24 23:36:52 -0700

    Update API URL in url.js to point to the test environment

--- database/migrations/2023_12_08_211745_create_ordenes_vw.php
+++ database/migrations/2023_12_08_211745_create_ordenes_vw.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -11,20 +12,21 @@ return new class extends Migration
      */
     public function up()
     {
+        DB::statement("DROP VIEW IF EXISTS ordenes_vw");
         DB::statement("
-            CREATE VIEW ordenes_vw AS
-            SELECT 
-                clientes.nombre AS nombre_cliente,
-                ordenes.mesa,
-                GROUP_CONCAT(CONCAT(platillos.nombre, '(', orden_platillos.cantidad, ')') ORDER BY platillos.nombre SEPARATOR ', ') AS platillos,
-                SUM(platillos.precio * orden_platillos.cantidad) AS precio_total,
-                ordenes.tipo_orden
-            FROM ordenes
-            JOIN orden_platillos ON ordenes.id = orden_platillos.orden_id
-            JOIN platillos ON orden_platillos.platillo_id = platillos.id
-            JOIN clientes ON ordenes.id_cliente = clientes.id
-            GROUP BY ordenes.id
-        ");
+        CREATE VIEW ordenes_vw AS
+        SELECT 
+            clientes.nombre AS nombre_cliente,
+            ordenes.mesa,
+            GROUP_CONCAT(CONCAT(platillos.nombre, '(', orden_platillos.cantidad, ')') ORDER BY platillos.nombre SEPARATOR ', ') AS platillos,
+            SUM(platillos.precio * orden_platillos.cantidad) AS precio_total,
+            ordenes.tipo_orden
+        FROM ordenes
+        JOIN orden_platillos ON ordenes.id = orden_platillos.orden_id
+        JOIN platillos ON orden_platillos.platillo_id = platillos.id
+        JOIN clientes ON ordenes.id_cliente = clientes.id
+        GROUP BY ordenes.id
+    ");
     }
 
     /**
--- resources/js/url.js
+++ resources/js/url.js
@@ -1,3 +1,4 @@
-const url = 'https://restaurante-api-production.up.railway.app/api/'
+// const url = 'https://restaurante-api-production.up.railway.app/api/'
+const url = 'https://test-main-kuylkg.laravel.cloud/api/'
 
 export default url
