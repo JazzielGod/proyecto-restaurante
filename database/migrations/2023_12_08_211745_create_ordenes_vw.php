ab21ccafbee5a4d847806bb28bbdeda461bd38c4
JazzielGod <jazzielbh@gmail.com>
2025-04-25 23:20:47 -0700

    refactor: actualizar la creación de la vista ordenes_vw para usar la sintaxis de concatenación adecuada

--- database/migrations/2023_12_08_211745_create_ordenes_vw.php
+++ database/migrations/2023_12_08_211745_create_ordenes_vw.php
@@ -13,19 +13,33 @@ return new class extends Migration
     public function up()
     {
         DB::statement("DROP VIEW IF EXISTS ordenes_vw");
-        DB::statement("
-        CREATE VIEW ordenes_vw AS
-        SELECT 
-            clientes.nombre AS nombre_cliente,
-            ordenes.mesa,
-            GROUP_CONCAT(CONCAT(platillos.nombre, '(', orden_platillos.cantidad, ')') ORDER BY platillos.nombre SEPARATOR ', ') AS platillos,
-            SUM(platillos.precio * orden_platillos.cantidad) AS precio_total,
-            ordenes.tipo_orden
-        FROM ordenes
-        JOIN orden_platillos ON ordenes.id = orden_platillos.orden_id
-        JOIN platillos ON orden_platillos.platillo_id = platillos.id
-        JOIN clientes ON ordenes.id_cliente = clientes.id
-        GROUP BY ordenes.id
+    //     DB::statement("
+    //     CREATE VIEW ordenes_vw AS
+    //     SELECT 
+    //         clientes.nombre AS nombre_cliente,
+    //         ordenes.mesa,
+    //         GROUP_CONCAT(CONCAT(platillos.nombre, '(', orden_platillos.cantidad, ')') ORDER BY platillos.nombre SEPARATOR ', ') AS platillos,
+    //         SUM(platillos.precio * orden_platillos.cantidad) AS precio_total,
+    //         ordenes.tipo_orden
+    //     FROM ordenes
+    //     JOIN orden_platillos ON ordenes.id = orden_platillos.orden_id
+    //     JOIN platillos ON orden_platillos.platillo_id = platillos.id
+    //     JOIN clientes ON ordenes.id_cliente = clientes.id
+    //     GROUP BY ordenes.id
+    // ");
+    DB::statement("
+    CREATE VIEW ordenes_vw AS
+SELECT 
+    clientes.nombre AS nombre_cliente,
+    ordenes.mesa,
+    GROUP_CONCAT(platillos.nombre || '(' || orden_platillos.cantidad || ')', ', ') AS platillos,
+    SUM(platillos.precio * orden_platillos.cantidad) AS precio_total,
+    ordenes.tipo_orden
+FROM ordenes
+JOIN orden_platillos ON ordenes.id = orden_platillos.orden_id
+JOIN platillos ON orden_platillos.platillo_id = platillos.id
+JOIN clientes ON ordenes.id_cliente = clientes.id
+GROUP BY ordenes.id;
     ");
     }
 
